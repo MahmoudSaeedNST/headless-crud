f19ace17be882c3f61cfe2fe0f00f38e8c32efb9
Mahmoud Saeed ㊧ᴋᴀᴋᴀsʜɪ <148659325+zeyadsayedel@users.noreply.github.com>
2025-04-11 12:47:20 +0200

    Implement user authentication, add API routes, and seed roles and permissions

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create roles
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'editor']);
+        Role::create(['name' => 'viewer']);
+        
+    }
+}
